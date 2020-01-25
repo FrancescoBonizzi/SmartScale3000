aba2d9beae625db188935d2ee7d49d2277053bb1
Francesco Bonizzi <fbonizzi.90@gmail.com>
2020-01-25 11:21:05 +0100

    Client bilancia bozza

---
+++ source/SmartWeightDevice/SmartWeightDevice/MainWindow.xaml.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Windows;
+
+namespace SmartWeightDevice
+{
+    public partial class MainWindow : Window
+    {
+        MainWindowViewModel _viewModel;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            _viewModel = new MainWindowViewModel();
+            DataContext = _viewModel;
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            _viewModel.Notifications.Add(DateTime.Now.ToString());
+        }
+    }
+}
---
+++ source/SmartWeightDevice/SmartWeightDevice/MainWindowViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.ObjectModel;
+
+namespace SmartWeightDevice
+{
+    public class MainWindowViewModel
+    {
+        public ObservableCollection<string> Notifications { get; set; }
+
+        public MainWindowViewModel()
+        {
+            Notifications = new ObservableCollection<string>();
+        }
+    }
+}
