59eecd63ea9d8d286518e9a6dbd860b001a6902b
Manuel <manueldeleo91@gmail.com>
2020-01-25 16:45:23 +0100

    Refactor model library code

--- source/MachineLearningPlayground/ImageClassification/ModelScorer/TFModelScorer.cs
+++ source/MachineLearningPlayground/ImageClassification/ModelScorer/TFModelScorer.cs
@@ -1,9 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
+﻿using System.Linq;
 using Microsoft.ML;
 using ImageClassification.ImageDataStructures;
-using static ImageClassification.ModelScorer.ConsoleHelpers;
 using static ImageClassification.ModelScorer.ModelHelpers;
 using System.Diagnostics;
 
@@ -11,21 +8,8 @@ namespace ImageClassification.ModelScorer
 {
     public class TFModelScorer
     {
-        private readonly string dataLocation;
-        private readonly string imagesFolder;
-        private readonly string modelLocation;
-        private readonly string labelsLocation;
         private readonly MLContext mlContext;
         private static string ImageReal = nameof(ImageReal);
-
-        public TFModelScorer(string dataLocation, string imagesFolder, string modelLocation, string labelsLocation)
-        {
-            this.dataLocation = dataLocation;
-            this.imagesFolder = imagesFolder;
-            this.modelLocation = modelLocation;
-            this.labelsLocation = labelsLocation;
-            mlContext = new MLContext();
-        }
 
         public TFModelScorer()
         {
@@ -42,74 +26,37 @@ namespace ImageClassification.ModelScorer
 
         public struct InceptionSettings
         {
-            // for checking tensor names, you can use tools like Netron,
-            // which is installed by Visual Studio AI Tools
-
-            // input tensor name
             public const string inputTensorName = "input";
-
-            // output tensor name
             public const string outputTensorName = "softmax2";
-        }
-
-        public void Score()
-        {
-            var model = LoadModel(dataLocation, imagesFolder, modelLocation);
-
-            var predictions = PredictDataUsingModel(dataLocation, imagesFolder, labelsLocation, model).ToArray();
-
         }
 
         public PredictionEngine<ImageNetData, ImageNetPrediction> LoadModel(string dataLocation, string imagesFolder, string modelLocation)
         {
-            ConsoleWriteHeader("Read model");
-            Console.WriteLine($"Model location: {modelLocation}");
-            Console.WriteLine($"Images folder: {imagesFolder}");
-            Console.WriteLine($"Training file: {dataLocation}");
-            Console.WriteLine($"Default parameters: image size=({ImageNetSettings.imageWidth},{ImageNetSettings.imageHeight}), image mean: {ImageNetSettings.mean}");
+            Debug.WriteLine($"Model location: {modelLocation}");
+            Debug.WriteLine($"Images folder: {imagesFolder}");
+            Debug.WriteLine($"Training file: {dataLocation}");
+            Debug.WriteLine($"Default parameters: image size=({ImageNetSettings.imageWidth},{ImageNetSettings.imageHeight}), image mean: {ImageNetSettings.mean}");
 
             var data = mlContext.Data.LoadFromTextFile<ImageNetData>(dataLocation, hasHeader: true);
 
-            var pipeline = mlContext.Transforms.LoadImages(outputColumnName: "input", imageFolder: imagesFolder, inputColumnName: nameof(ImageNetData.ImagePath))
-                            .Append(mlContext.Transforms.ResizeImages(outputColumnName: "input", imageWidth: ImageNetSettings.imageWidth, imageHeight: ImageNetSettings.imageHeight, inputColumnName: "input"))
-                            .Append(mlContext.Transforms.ExtractPixels(outputColumnName: "input", interleavePixelColors: ImageNetSettings.channelsLast, offsetImage: ImageNetSettings.mean))
-                            .Append(mlContext.Model.LoadTensorFlowModel(modelLocation).
-                            ScoreTensorFlowModel(outputColumnNames: new[] { "softmax2" },
-                                                inputColumnNames: new[] { "input" }, addBatchDimensionInput:true));
-                        
+            var pipeline = mlContext.Transforms.LoadImages(
+                outputColumnName: "input",
+                imageFolder: imagesFolder,
+                inputColumnName: nameof(ImageNetData.ImagePath))
+                    .Append(mlContext.Transforms.ResizeImages(
+                        outputColumnName: "input", imageWidth: ImageNetSettings.imageWidth, imageHeight: ImageNetSettings.imageHeight, inputColumnName: "input"))
+                    .Append(mlContext.Transforms.ExtractPixels(
+                        outputColumnName: "input", interleavePixelColors: ImageNetSettings.channelsLast, offsetImage: ImageNetSettings.mean))
+                    .Append(mlContext.Model.LoadTensorFlowModel(modelLocation).ScoreTensorFlowModel(
+                        outputColumnNames: new[] { "softmax2" },
+                        inputColumnNames: new[] { "input" },
+                        addBatchDimensionInput: true));
+
             ITransformer model = pipeline.Fit(data);
 
             var predictionEngine = mlContext.Model.CreatePredictionEngine<ImageNetData, ImageNetPrediction>(model);
 
             return predictionEngine;
-        }
-
-        protected IEnumerable<ImageNetData> PredictDataUsingModel(string testLocation, 
-                                                                  string imagesFolder, 
-                                                                  string labelsLocation, 
-                                                                  PredictionEngine<ImageNetData, ImageNetPrediction> model)
-        {
-            ConsoleWriteHeader("Classificate images");
-            Console.WriteLine($"Images folder: {imagesFolder}");
-            Console.WriteLine($"Training file: {testLocation}");
-            Console.WriteLine($"Labels file: {labelsLocation}");
-
-            var labels = ModelHelpers.ReadLabels(labelsLocation);
-
-            var testData = ImageNetData.ReadFromCsv(testLocation, imagesFolder);
-
-            foreach (var sample in testData)
-            {
-                var probs = model.Predict(sample).PredictedLabels;
-                var imageData = new ImageNetDataProbability()
-                {
-                    ImagePath = sample.ImagePath,
-                    Label = sample.Label
-                };
-                (imageData.PredictedLabel, imageData.Probability) = GetBestLabel(labels, probs);
-                imageData.ConsoleWrite();
-                yield return imageData;
-            }
         }
 
         public ImageNetDataProbability PredictSingleImageDataUsingModel(
@@ -129,7 +76,6 @@ namespace ImageClassification.ModelScorer
                 Label = string.Empty
             };
             (imageData.PredictedLabel, imageData.Probability) = GetBestLabel(labels, probs);
-            imageData.ConsoleWrite();
             return imageData;
         }
     }
